bf44680c5663df2c4485fff85222ef4f7b7f2061
Obida Sabbagh <tiger3homs@gmail.com>
2025-09-16 21:47:14 +0200

    Create deathmatchmode.js

---
+++ public/deathmatchmode.js
@@ -0,0 +1,45 @@
+(() => {
+  'use strict';
+  const servers = document.querySelectorAll('[data-server]');
+  for (let i = 0; i < servers.length; i++) {
+    const server = servers[i];
+    const serverID = server.dataset.server;
+    const settings = server.nextElementSibling.nextElementSibling;
+
+    // Public server ON
+    server.querySelector(`input[id="server[${serverID}][public]"]`).checked = true;
+
+    // Reset all checkboxes
+    const varsCheckboxes = settings.querySelectorAll('input[type="checkbox"]');
+    for (let k = 0; k < varsCheckboxes.length; k++) {
+      varsCheckboxes[k].checked = false;
+    }
+
+    // Public defaults
+    settings.querySelector(`input[id="server[${serverID}][cvars][mp_friendlyfire]"]`).checked = false;
+    settings.querySelector(`input[id="server[${serverID}][cvars][mp_autoteambalance]"]`).checked = true;
+    settings.querySelector(`input[id="server[${serverID}][cvars][mp_afkbomb]"]`).checked = true;
+    settings.querySelector(`input[id="server[${serverID}][cvars][afk_kick]"]`).checked = true;
+    settings.querySelector(`input[id="server[${serverID}][cvars][statistics]"]`).checked = true;
+    settings.querySelector(`input[id="server[${serverID}][cvars][votekick]"]`).checked = true;
+    settings.querySelector(`input[id="server[${serverID}][cvars][bonus_slot]"]`).checked = true;
+    settings.querySelector(`input[id="server[${serverID}][cvars][tfb]"]`).checked = true;
+    settings.querySelector(`input[id="server[${serverID}][cvars][statsx]"]`).checked = true;
+    settings.querySelector(`input[id="server[${serverID}][cvars][dib3]"]`).checked = true;
+    settings.querySelector(`input[id="server[${serverID}][cvars][rwd_grenadedrop]"]`).checked = true;
+
+
+    // Dropdown defaults
+    settings.querySelector(`select[name="server[${serverID}][cvars][minimal_skill]"]`).value = '0';
+    settings.querySelector(`select[name="server[${serverID}][cvars][ping_limit]"]`).value = '250';
+    settings.querySelector(`select[name="server[${serverID}][cvars][mp_roundtime]"]`).value = '2.5';
+    settings.querySelector(`select[name="server[${serverID}][cvars][mp_buytime]"]`).value = '0.5';
+    settings.querySelector(`select[name="server[${serverID}][cvars][mp_c4timer]"]`).value = '35';
+    settings.querySelector(`select[name="server[${serverID}][cvars][mp_freezetime]"]`).value = '5';
+    settings.querySelector(`select[name="server[${serverID}][cvars][mp_startmoney]"]`).value = '1000';
+    settings.querySelector(`select[name="server[${serverID}][cvars][csem_sank_cd]"]`).value = '300';
+    settings.querySelector(`select[name="server[${serverID}][cvars][limit_hegren]"]`).value = '1';
+    settings.querySelector(`select[name="server[${serverID}][cvars][limit_sgren]"]`).value = '1';
+    settings.querySelector(`select[name="server[${serverID}][cvars][limit_flash]"]`).value = '2';
+  }
+})();
